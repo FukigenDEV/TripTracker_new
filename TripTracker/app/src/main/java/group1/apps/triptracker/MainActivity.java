c380ce8054a797998a6f1261cd9c7dd782821f00
FukigenDEV <mikevdburg@live.nl>
2019-04-23 21:01:03 +0200

    Add new design

--- TripTracker/app/src/main/java/group1/apps/triptracker/MainActivity.java
+++ TripTracker/app/src/main/java/group1/apps/triptracker/MainActivity.java
@@ -1,13 +1,33 @@
 package group1.apps.triptracker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView btnAddMemory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAddMemory = findViewById(R.id.btnAddMemory);
+
+        btnAddMemory.setOnClickListener(getOnClickListener());
+    }
+
+    private View.OnClickListener getOnClickListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v == btnAddMemory) {
+                    startActivity(new Intent(MainActivity.this, AddMemoryActivity.class));
+                }
+            }
+        };
     }
 }
