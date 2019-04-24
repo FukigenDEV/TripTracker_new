5fc2999884bf2e6d92cff995e5cff25dd5f92652
nhazeu1 <47557049+nhazeu1@users.noreply.github.com>
2019-04-24 14:29:24 +0200

    Created database
    Created the database and added 1 table to save the memory data.

--- TripTracker/app/src/main/java/group1/apps/triptracker/MainActivity.java
+++ TripTracker/app/src/main/java/group1/apps/triptracker/MainActivity.java
@@ -1,14 +1,21 @@
 package group1.apps.triptracker;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private ImageView btnAddMemory;
+    private MemoryDbHelper mMemoryDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +24,64 @@ public class MainActivity extends AppCompatActivity {
 
         btnAddMemory = findViewById(R.id.btnAddMemory);
 
+        mMemoryDbHelper = new MemoryDbHelper(this);
+
         btnAddMemory.setOnClickListener(getOnClickListener());
+
+        insertTestValues();
+        retrieveTestValues();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mMemoryDbHelper.close();
+
+        super.onDestroy();
+    }
+
+    private void insertTestValues() {
+        SQLiteDatabase db = mMemoryDbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(MemoryContract.MemoryEntry.COLUMN_NAME_TITLE, "Memory title");
+        values.put(MemoryContract.MemoryEntry.COLUMN_NAME_DESCRIPTION, "Memory description");
+
+        db.insert(MemoryContract.MemoryEntry.TABLE_NAME, null, values);
+    }
+
+    private List<String> retrieveTestValues() {
+        SQLiteDatabase db = mMemoryDbHelper.getReadableDatabase();
+
+        Cursor cursor = db.query(
+                MemoryContract.MemoryEntry.TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+        List<Long> itemIds = new ArrayList<>();
+        List<String> itemTitles = new ArrayList<>();
+
+        while (cursor.moveToNext()) {
+            long itemId = cursor.getLong(cursor.getColumnIndexOrThrow(MemoryContract.MemoryEntry._ID));
+            String itemTitle = cursor.getString(cursor.getColumnIndexOrThrow(MemoryContract.MemoryEntry.COLUMN_NAME_TITLE));
+
+            itemIds.add(itemId);
+            itemTitles.add(itemTitle);
+        }
+
+        cursor.close();
+
+        return itemTitles;
+    }
+
+    private void deleteTableData() {
+        SQLiteDatabase db = mMemoryDbHelper.getWritableDatabase();
+
+        db.execSQL("DELETE FROM " + MemoryContract.MemoryEntry.TABLE_NAME);
     }
 
     private View.OnClickListener getOnClickListener() {
---
+++ TripTracker/app/src/main/java/group1/apps/triptracker/MemoryContract.java
@@ -0,0 +1,31 @@
+package group1.apps.triptracker;
+
+import android.provider.BaseColumns;
+
+public final class MemoryContract {
+
+    private MemoryContract() {
+
+    }
+
+    public static class MemoryEntry implements BaseColumns {
+        public static final String TABLE_NAME = "memories";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_DESCRIPTION = "description";
+        public static final String COLUMN_NAME_DATE_ADDED = "date_added";
+        public static final String COLUMN_NAME_IMAGE = "image";
+        public static final String COLUMN_NAME_LOCATION = "location";
+    }
+
+    public static final String SQL_CREATE_MEMORIES =
+            "CREATE TABLE " + MemoryEntry.TABLE_NAME + " (" +
+                    MemoryEntry._ID + " INTEGER PRIMARY KEY," +
+                    MemoryEntry.COLUMN_NAME_TITLE + " TEXT," +
+                    MemoryEntry.COLUMN_NAME_DESCRIPTION + " TEXT," +
+                    MemoryEntry.COLUMN_NAME_DATE_ADDED + " TEXT," +
+                    MemoryEntry.COLUMN_NAME_IMAGE + " TEXT," +
+                    MemoryEntry.COLUMN_NAME_LOCATION + " TEXT)";
+
+    public static final String SQL_DELETE_MEMORIES =
+            "DROP TABLE IF EXISTS " + MemoryEntry.TABLE_NAME;
+}
