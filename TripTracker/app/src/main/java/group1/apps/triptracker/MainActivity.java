9c9afa31a43921f0e6d087b329f02e33d88ce355
nhazeu1 <47557049+nhazeu1@users.noreply.github.com>
2019-05-27 16:24:03 +0200

    bs

--- TripTracker/app/src/main/java/group1/apps/triptracker/MainActivity.java
+++ TripTracker/app/src/main/java/group1/apps/triptracker/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 import java.util.List;
